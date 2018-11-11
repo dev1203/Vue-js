4972c5493a17e8b624f5d01481b9246563b5dee2
dev1203 <devsingh120397@gmail.com>
2018-11-10 17:17:06 -0800

    Some more practise Material

--- first/script.js
+++ first/script.js
@@ -1,11 +1,18 @@
-new Vue({
-    el : '#app',
+
+new Vue({ //Creating a new Vue object
+    el : '#app',    // Element we need to manupulate    
     data : {
-        title : 'Test Data'
+        title : 'Test Data' ,
+        link : 'http://www.whodev.info',
+        finishedlink : `<a href ="https://www.google.com">google</a>`
     },
     methods : {
-        changeTitle : function(event){
+        changeTitle : function(event){  //All the methods added in the html
             this.title = event.target.value
+        },
+        sayHello : function () {
+            this.title = 'hahah'
+            return this.title;
         }
     }
 })
